b83af30f8ae8182f79aa93a05a1d597f90b2bd87
Nate Kaufman <kaufman.nathaniel@gmail.com>
2014-09-19 20:53:14 -0500

    way to call some json

---
+++ getinfo.js
@@ -0,0 +1,13 @@
+//GET /groups
+//This returns the ith object's amount of messages.
+var groups = "Array of json obj";
+groups[i].mesages.count;
+
+//GET /users/me
+//This gets the name to put at the top of the page
+var user = "json obj";
+user.name;
+
+//GET /direct_messages
+var dm = "json obj;
+dm.count
