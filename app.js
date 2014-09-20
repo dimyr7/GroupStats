ec817c0b3472b1ed9ba1bda85438005a79a1c969
btmoore95 <BenooreM@wirelessprvnat-172-17-125-226.near.illinois.edu>
2014-09-20 14:26:15 -0500

    asdf
    adfs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var x = getParameterByName("access_token"); 
+console.log(x); 
 if (x == undefined){
   window.location="https://oauth.groupme.com/oauth/authorize?client_id=HvgtfdoiyqVpz9fuHrBGrnb0vCmw0VXnMkXL1cG91NtoETFf";
 }
