b8ca6d3f4a3ac9cc88c8ca6d4d2c9ac26f011f99
Dmitry Myronenko <dimyr7@hotmail.com>
2014-09-20 06:14:47 -0500

    Update recentMessages.js

--- recentMessages.js
+++ recentMessages.js
@@ -1,7 +1,117 @@
-//GET /groups
-//This returns the ith object's amount of messages.
-//Leave at default to get 10 groups
-var groups = $.get("https://api.groupme.com/v3/groups?token=YOUR_ACCESS_TOKEN"), {page:1, per_page:10};
+function getTopTen(){
+	var gmurl = "https://api.groupme.com/v3/";
+	var token = "?token=bcb9df401b470132915c464450d26530";
+	var allGroups = [];
+	var allMesseges = [];
+	var finalMess = [];
+	var req = new XMLHttpRequest();
+	req.open('GET', gmurl+"groups"+token, false);
+	req.send(null);
+	if(req.status==200){
+		var s = JSON.parse(req.responseText);
+		for(var i = 0; i < s.response.length; i++){
+			allGroups.push(s.response[i].id);
+		}
+		
+	}
+	/*for(var i =0; i <allGroups.length; i++){
+		console.log(allGroups[i]);
+	}*/
+	for(var i = 0; i < allGroups.length; i++){
+		req.open('GET', gmurl+'groups/'+allGroups[i]+'/messages'+token, false);
+		req.send(null);
+		if(req.status==200){
+			var s = JSON.parse(req.responseText);
+			for(var j = 0; j < s.response.messages.length; j++){
+				allMesseges.push(s.response.messages[j]);
+			}
+			console.log("good");
+		}
+	}
+	allMesseges.sort(function(a,b){
+		return b.created_at-a.created_at;
+	});
+	for(var i = 0; i < allMesseges.length; i++){
+		//console.log(allMesseges[i].text + (new Date(allMesseges[i].created_at*1000)));
+	}
+	for(var i = 0; i < 10; i++){
+		finalMess[i]=allMesseges[i];
+	}
+	for(var i = 0; i < finalMess.length; i++){
+		console.log(finalMess[i].text+(new Date(finalMess[i].created_at*1000)));
+	}
+
+});
+/*
+	$.getJSON(gmurl+"groups"+token, function(data){
+		var allGroups = [];
+		//console.log(data.response.length);
+		for(var i = 0; i < data.response.length; i++){
+			allGroups.push(data.response[i]);	
+			//console.log(allGroups[i].id);
+		}
+		// Prints 1 group IDs
+		/*for(var i = 0; i < allGroups.length; i++){
+			console.log(allGroups[i].id);
+		}
+		
+		console.log(getRecentMess(0));
+		function getRecentMess(groupnum){
+			$.getJSON(gmurl+"groups/"+allGroups[groupnum].id+"/messages"+token,  function(data2){	
+				var allMesseges = [];
+				for(var j = 0; j < data2.response.messages.length;j++){
+					var temp = data2.response.messages[j];
+					allMesseges.push(temp.text);
+				}
+				/*allMesseges.sort(function(a,b){
+					return b[1]-a[1];
+				});
+				return allMesseges[0];
+				//for(var j = 0; j < allMesseges.length; j++){
+					//console.log(allMesseges.length);
+					//console.log(allMesseges[0]+allMesseges[1]+allMesseges[2]);
+				//}	
+			});
+		}		
+		
+	});
+
+});
+*/
+/*
+	$.get(url+"groups"+token, function(data){
+		var groupID = [];
+		for(var i = 0; i < data.response.length; i++){
+			var group = data.response[i];
+			groupID.push(group.group_id);
+		}
+		var messages = [];
+		for(var i = 0; i < groupID.length; i++){
+			//alert(groupID[i]);
+			var str = url+"groups/"+groupID[i]+"/messages"+token;
+			$.get(url+"groups/"+groupID[i]+"/messages"+token,  function(data2){
+				for(var j = 0; j < data2.response.messages.length; j++){
+					messages.push(data2.response.messages[j]);
+				}
+				console.log(i);
+					//console.log("hello")
+					messages.sort(function(a,b){
+						return b.created_at - a.created_at;
+					});
+					for(var k = 0; k < messages.length; j++){
+					var c = new Date(messages[j].created_at*1000);
+					console.log(c);
+					console.log(messages[k].text + messages[k].created_at);
+					}
+			});	
+
+			
+			
+		}
+		
+	});
+});
+
 
 
 var groupID = ["1","2","3","4","5","6","7","8","9","10"];
@@ -13,7 +123,7 @@ for (i=0;i<10;i++)
 {
 	groupID[i] = groups.response[i].id;
 }
-
+ocu
 
 //group_id = groupID[0];
 //GET /groups/:group_id/messages !!LIMIT_PARAM = 10!!
@@ -48,7 +158,7 @@ for (c = 0 ; c < ( n - 1 ); c++)
 {
 	for (d = 0 ; d < n - c - 1; d++)
 	{
-		if (topTenMesCA[d] > topTenMesCA[d+1]) /* For decreasing order use < */
+		if (topTenMesCA[d] > topTenMesCA[d+1]) /* For decreasing order use < 
 		{
 			swap = topTenMesCA[d];
 			topTenMesCA[d] = topTenMesCA[d+1];
@@ -69,30 +179,4 @@ for (c = 0 ; c < ( n - 1 ); c++)
 	}
 }
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
