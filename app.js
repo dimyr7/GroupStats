c94f64b4196b55b78123f937e654eb7f5f7c0ed5
Dima M <myroenen2@illinois.edu>
2014-09-20 07:50:08 -0500

    quote

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 var top_ten = getTopTen();
-console.log(top_ten);
+var top_groups = getTopTenGroups();
+//console.log(top_ten);
 var app = angular.module('stats', []);
 app.controller('user', ['$scope', function($scope) {
   $scope.username = 'btmoore95';
@@ -8,46 +9,46 @@ app.controller('user', ['$scope', function($scope) {
 app.controller('M1', ['$scope', function($scope) {
   $scope.text = top_ten[0].text;
   $scope.name = top_ten[0].name;
-  $scope.group = 'Lakehouse'; 
+  $scope.group = (new Date(top_ten[0].created_at*1000).toString().substring(0,24)); 
 }]);
 app.controller('M2', ['$scope', function($scope) {
   $scope.text = top_ten[1].text;
   $scope.name = top_ten[1].name;
-  $scope.group = '4TS'; 
+  $scope.group = (new Date(top_ten[1].created_at*1000).toString().substring(0,24));
 }]);
 app.controller('M3', ['$scope', function($scope) {
   $scope.text = top_ten[2].text;
   $scope.name = top_ten[2].name;
-  $scope.group = 'Lakehouse'; 
+  $scope.group = (new Date(top_ten[2].created_at*1000).toString().substring(0,24)); 
 }]);
 app.controller('M4', ['$scope', function($scope) {
   $scope.text = top_ten[3].text;
   $scope.name = top_ten[3].name;
-  $scope.group = 'Right Now'; 
+  $scope.group = (new Date(top_ten[3].created_at*1000).toString().substring(0,24));
 }]);
 app.controller('M5', ['$scope', function($scope) {
   $scope.text = top_ten[4].text;
   $scope.name = top_ten[4].name;
-  $scope.group = 'Fucked Up'; 
+  $scope.group = (new Date(top_ten[4].created_at*1000).toString().substring(0,24));
 }]);
 app.controller('M6', ['$scope', function($scope) {
   $scope.text = top_ten[5].text;
   $scope.name = top_ten[5].name;
-  $scope.group = 'The OGs'; 
+  $scope.group = (new Date(top_ten[5].created_at*1000).toString().substring(0,24));
 }]);
 app.controller('M7', ['$scope', function($scope) {
   $scope.text = top_ten[6].text;
   $scope.name = top_ten[6].name;
-  $scope.group = 'Hackathon'; 
+  $scope.group = (new Date(top_ten[6].created_at*1000).toString().substring(0,24));
 }]);
 app.controller('M8', ['$scope', function($scope) {
   $scope.text = top_ten[7].text;
   $scope.name = top_ten[7].name;
-  $scope.group = 'Who is Doug'; 
+  $scope.group = (new Date(top_ten[7].created_at*1000).toString().substring(0,24));
 }]);
 app.controller('M9', ['$scope', function($scope) {
   $scope.text = top_ten[8].text;
   $scope.name = top_ten[8].name;
-  $scope.group = 'nosleep'; 
+  $scope.group = (new Date(top_ten[8].created_at*1000).toString().substring(0,24));
 }]);
 
--- recentMessages.js
+++ recentMessages.js
@@ -4,6 +4,7 @@ function getTopTen(){
 	var allGroups = [];
 	var allMesseges = [];
 	var finalMess = [];
+	var finalGroup = [];
 	var req = new XMLHttpRequest();
 	req.open('GET', gmurl+"groups"+token, false);
 	req.send(null);
@@ -25,7 +26,7 @@ function getTopTen(){
 			for(var j = 0; j < s.response.messages.length; j++){
 				allMesseges.push(s.response.messages[j]);
 			}
-			console.log("good");
+			//console.log("good");
 		}
 	}
 	allMesseges.sort(function(a,b){
@@ -38,145 +39,34 @@ function getTopTen(){
 		finalMess[i]=allMesseges[i];
 	}
 	for(var i = 0; i < finalMess.length; i++){
-		console.log(finalMess[i].text+(new Date(finalMess[i].created_at*1000)));
+		//console.log(finalGroup[i]);
 	}
 	return finalMess; 
 };
 /*
-	$.getJSON(gmurl+"groups"+token, function(data){
-		var allGroups = [];
-		//console.log(data.response.length);
-		for(var i = 0; i < data.response.length; i++){
-			allGroups.push(data.response[i]);	
-			//console.log(allGroups[i].id);
-		}
-		// Prints 1 group IDs
-		/*for(var i = 0; i < allGroups.length; i++){
-			console.log(allGroups[i].id);
-		}
-		
-		console.log(getRecentMess(0));
-		function getRecentMess(groupnum){
-			$.getJSON(gmurl+"groups/"+allGroups[groupnum].id+"/messages"+token,  function(data2){	
-				var allMesseges = [];
-				for(var j = 0; j < data2.response.messages.length;j++){
-					var temp = data2.response.messages[j];
-					allMesseges.push(temp.text);
-				}
-				/*allMesseges.sort(function(a,b){
-					return b[1]-a[1];
-				});
-				return allMesseges[0];
-				//for(var j = 0; j < allMesseges.length; j++){
-					//console.log(allMesseges.length);
-					//console.log(allMesseges[0]+allMesseges[1]+allMesseges[2]);
-				//}	
-			});
+function getTopTenGroups(){
+	var gmurl = "https://api.groupme.com/v3/";
+	var token = "?token=bcb9df401b470132915c464450d26530";
+	var mess = getTopTen();
+	var allGroups = [];
+	
+	for(var i = 2; i < 3; i++){
+		var req = new XMLHttpRequest();
+		console.log("ID:"+mess[i].group_id);
+		req.open('GET', gmurl+'groups/'+token+'&id='+mess[i].group_id , false);
+		req.send(null);
+		if(req.status==200){
+			//console.log("good");
+
+			var s =JSON.parse(req.responseText);
+			console.log(mess[i].group_id + mess[i].text);
+			console.log(JSON.stringify(s));
+			//console.log(s.response.name);
+			allGroups.push(s.response.created_at);
 		}		
-		
-	});
-
-});
-*/
-/*
-	$.get(url+"groups"+token, function(data){
-		var groupID = [];
-		for(var i = 0; i < data.response.length; i++){
-			var group = data.response[i];
-			groupID.push(group.group_id);
-		}
-		var messages = [];
-		for(var i = 0; i < groupID.length; i++){
-			//alert(groupID[i]);
-			var str = url+"groups/"+groupID[i]+"/messages"+token;
-			$.get(url+"groups/"+groupID[i]+"/messages"+token,  function(data2){
-				for(var j = 0; j < data2.response.messages.length; j++){
-					messages.push(data2.response.messages[j]);
-				}
-				console.log(i);
-					//console.log("hello")
-					messages.sort(function(a,b){
-						return b.created_at - a.created_at;
-					});
-					for(var k = 0; k < messages.length; j++){
-					var c = new Date(messages[j].created_at*1000);
-					console.log(c);
-					console.log(messages[k].text + messages[k].created_at);
-					}
-			});	
-
-			
-			
-		}
-		
-	});
-});
-
-
-
-var groupID = ["1","2","3","4","5","6","7","8","9","10"];
-var topTenMesName = ["1","2","3","4","5","6","7","8","9","10"];
-var topTenMesText = ["1","2","3","4","5","6","7","8","9","10"];
-var topTenMesGroup = ["1","2","3","4","5","6","7","8","9","10"];
-var topTenMesCA = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
-for (i=0;i<10;i++)
-{
-	groupID[i] = groups.response[i].id;
-}
-ocu
-
-//group_id = groupID[0];
-//GET /groups/:group_id/messages !!LIMIT_PARAM = 10!!
-var mess = "json OBJ";
-
-
-for(int i=0;i<10;i++)
-{
-	topTenMesCA[i] = mess.messages[i].created_at;
-	topTenMesName[i] = mess.messages[i].name;
-	topTenMesText[i] = mess.messages[i].text;
-	topTenMesGroup[i] = groups.response[0].name;
-}
-
-for(i=1;i<10;i++)
-{
-	//group_id = groupID[i];
-	//GET /groups/:group_id/messages !!LIMIT_PARAM = 10!!
-	var mess = "json obj";
-	for (h=0;h<10;h++)
-	{
-		if (mess.messages[h].created_at<topTenMesCA[h])
-		{
-			topTenMesCA[h] = mess.messages[h].created_at;
-			topTenMesName[h] = mess.messages[h].name;
-			topTenMesText[h] = mess.messages[h].text;
-			topTenMesGroup[h] = groups.response[i].name;
-		}
 	}
-}
-for (c = 0 ; c < ( n - 1 ); c++)
-{
-	for (d = 0 ; d < n - c - 1; d++)
-	{
-		if (topTenMesCA[d] > topTenMesCA[d+1]) /* For decreasing order use < 
-		{
-			swap = topTenMesCA[d];
-			topTenMesCA[d] = topTenMesCA[d+1];
-			topTenMesCA[d+1] = swap;
-			
-			swap = topTenMesName[d];
-			topTenMesName[d] = topTenMesName[d+1];
-			topTenMesName[d+1] = swap;
-			
-			swap = topTenMesText[d];
-			topTenMesText[d] = topTenMesText[d+1];
-			topTenMesText[d+1] = swap;
-			
-			swap = topTenMesGroup[d];
-			topTenMesGroup[d] = topTenMesGroup[d+1];
-			topTenMesGroup[d+1] = swap;
-		}
+	for(var i = 0; i < 10; i++){
+		//console.log((new Date(allGroups[i])*1000));
 	}
-}
- 
-*/
+
+};*/
